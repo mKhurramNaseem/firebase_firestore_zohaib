5f0144d9ac9cec6cb88edbffe63f9c4867583bfd
mKhurramNaseem <naseemkhurram635@gmail.com>
2025-01-21 12:22:28 +0500

    Title Changed

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My App  Home'),
     );
   }
 }
